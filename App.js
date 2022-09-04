168ab8034f98ce960e4f641311b5335c40827669
Matias Aristimuno <mattaristi@MacBook-Air-de-Ariel.local>
2022-09-04 20:41:19 +0100

    RNative

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+
   },
+  inputContainer:{
+    borderRadius:10,
+    marginTop: 30,
+    marginBottom: 20,
+    marginHorizontal: 20
+  },
+  input: {
+    borderBottomColor: '0A0908',
+    borderBottomWidth: 2,
+    height: 40
+  },
+  button: {
+    backgroundColor:'0A0908',
+    color: '#fffff',
+    height:35,
+    width:65
+  }
+
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='text' style={styles.input}/>
+        <Button title='add' style={styles.button} onPress={( ) => {console.warn('hola')}} />
+      </View>
+    </View>
+  );
+}
+
